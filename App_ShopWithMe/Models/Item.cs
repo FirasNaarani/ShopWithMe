94575543f182f9141c634f725e95dcc0feb66bdc
YakirPanker <89133267+YakirPanker@users.noreply.github.com>
2021-10-10 15:32:34 +0300

    fix

---
+++ App_ShopWithMe/Models/Item.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace App_ShopWithMe.Models
+{
+    public class Item
+    {
+
+        [JsonProperty(PropertyName = "id")]
+        public string Id { get; set; }
+
+ 
+        [JsonProperty(PropertyName = "name")]
+        [Required]
+        public string Name { get; set; }
+
+        [JsonProperty(PropertyName = "type")]
+        [Required]
+        public string Type { get; set; }
+
+        [JsonProperty(PropertyName = "price")]
+        [Required]
+        public int Price { get; set; }
+
+        [JsonProperty(PropertyName = "url")]
+        [Required]
+        public string Url { get; set; }
+        
+        //public Item(string id, string name)
+        //{
+        //    Id = id;
+        //    Name = name;
+        //}
+    }
+}
